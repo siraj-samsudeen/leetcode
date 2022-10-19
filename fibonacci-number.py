507afba4914e6638b50a17b1c6c9804aab208eb6
Siraj Samsudeen <mailsiraj@gmail.com>
2022-10-19 09:50:59 +0530

    fibonacci rep2

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -1,30 +1,32 @@
+# Fibonacci Number - LeetCode
+# https://leetcode.com/problems/fibonacci-number/
 class Solution:
 #     DP style
     def fib(self, n: int) -> int:
         if n < 2:
             return n
-
-        a = 0
-        b = 1
         
-        for i in range(2,n+1):
+        a, b = 0, 1
+        
+        for i in range(2, n+1):
             c = a + b
             a = b
-            b =c
+            b = c
         
         return c
-
+    
 #     recursive
     def fib(self, n: int) -> int:
         if n < 2:
             return n
         
-        return self.fib(n-2) + self.fib(n-1)
+        return self.fib(n-1) + self.fib(n-2)
 
 #     clever version of DP style without a 3rd variable
     def fib(self, n: int) -> int:
-        if  n < 2: return n
+        if n < 2: return n
         a, b = 0, 1
+        
         for _ in range(1, n):
             a, b = b, a+b
         return b
